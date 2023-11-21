c154b264da23d952b06408a99b5fcb30226607bd
Andrew <andrewozo123@gmail.com>
2023-11-21 13:30:11 -0500

    added rest of CRUD operations

--- src/app.js
+++ src/app.js
@@ -5,10 +5,65 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
 
-app.get("/restaurants", async (req, res) => {
-  const restaurants = await Restaurant.findAll();
+app.use(express.json());
+app.use(express.urlencoded());
 
-  res.json(restaurants);
+app.get("/restaurants", async (req, res, next) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+
+    res.json(restaurants);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/restaurants/:id", async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+
+    res.json(restaurant);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/restaurants", async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.create(req.body);
+
+    res.json(restaurant);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put("/restaurants/:id", async (req, res, next) => {
+  try {
+    const updated = await Restaurant.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (updated[0] === 0) {
+      throw new Error("Update not completed");
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/restaurants/:id", async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+
+    await restaurant.destroy();
+
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = app;
