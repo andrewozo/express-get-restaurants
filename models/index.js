3c56594f79e3a5a109b478f6fc5d2c555c22b987
Andrew <andrewozo123@gmail.com>
2023-11-24 09:58:06 -0500

    created associations

--- models/index.js
+++ models/index.js
@@ -2,6 +2,12 @@ const Restaurant = require("./Restaurant");
 const Menu = require("./Menu");
 const Item = require("./Item");
 
+Restaurant.hasMany(Menu);
+Menu.belongsTo(Restaurant);
+
+Item.belongsToMany(Menu, { through: "menuItems" });
+Menu.belongsToMany(Item, { through: "menuItems" });
+
 module.exports = {
   Restaurant,
   Menu,
